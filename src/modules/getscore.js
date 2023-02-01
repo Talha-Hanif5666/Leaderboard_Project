2c17bb436957035557aa49f1200e8446522bc31e
Muhammad Talha Hanif <talhahanif5666@gmail.com>
2023-02-01 21:45:15 +0500

    Adding module files

---
+++ src/modules/getscore.js
@@ -0,0 +1,17 @@
+import { scoreDiv } from './declare.js';
+// Game id kN0uEuCAtmzgUqRYuCdv
+const getScores = async () => {
+  const scores = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/kN0uEuCAtmzgUqRYuCdv/scores/');
+  const res = await scores.json();
+  const strObj = JSON.stringify(res.result);
+  const parseObj = JSON.parse(strObj);
+
+  parseObj.forEach((obj) => {
+    scoreDiv.innerHTML += `
+    <tr>
+    <td>${obj.user}</td> 
+    <td> ${obj.score}</td><br></tr>`;
+  });
+};
+
+export default getScores;
